349489d50391045c7711297eea0dd472e2878d99
Yaya <sikiruyaya1@gmail.com>
2023-11-01 08:47:05 -0700

    calculator dependencies and components

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import Layout from "./components/layout"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout></Layout>
+    </div>
+  );
+}
+
+export default App;
